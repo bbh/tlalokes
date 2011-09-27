493f8354ee58a6eb667d1459fbc8fc0054ae22f0
Basilio Briceño H <bbh@tlalokes.org>
2011-09-26 21:06:56 -0500

    disclaimer added

--- framework/functions.php
+++ framework/functions.php
@@ -1,5 +1,24 @@
 <?php
 /**
+ * Tlalokes framework functions
+ * Copyright (c) 2011 Basilio Briceno <bbh@tlalokes.org>
+ *
+ * This file is part of the Tlalokes framework.
+ *
+ * Tlalokes framework is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, version 3 of the License.
+ *
+ * Tlalokes framework is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this file. If not, see <http://www.gnu.org/licenses/lgpl.html>.
+ */
+
+/**
  * Loads Tlalokes framework
  *
  * @author Basilio Briceno <bbh@tlalokes.org>
